e40fc9d6a6bcf37f5537a7a223cc4e083e3e8462
Wang Jie <i@i8e.net>
2026-04-03 03:37:01 +0800

    save my mac

---
+++ dot_hammerspoon/init.lua
@@ -0,0 +1,17 @@
+local function pressFn(mods, key)
+  if key == nil then
+    key = mods
+    mods = {}
+  end
+
+  return function() hs.eventtap.keyStroke(mods, key, 1000) end
+end
+
+local function remap(mods, key, pressFn)
+  hs.hotkey.bind(mods, key, pressFn, nil, pressFn)
+end
+
+remap({ 'ctrl' }, 'h', pressFn('left'))
+remap({ 'ctrl' }, 'j', pressFn('down'))
+remap({ 'ctrl' }, 'k', pressFn('up'))
+remap({ 'ctrl' }, 'l', pressFn('right'))
